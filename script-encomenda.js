5ab46462b409323aa674aa636144612008544e86
nekodany <danyellefhipolito@gmail.com>
2026-09-17 13:20:38 -0300

    Update script-encomenda.js

--- script-encomenda.js
+++ script-encomenda.js
@@ -1,18 +1,11 @@
+alert("SCRIPT DA ENCOMENDA CARREGOU!");
+
 const formulario = document.getElementById("formulario-plantas");
 
 formulario.addEventListener("submit", function(event) {
 
     event.preventDefault();
 
-    const nome = document.getElementById("nome").value;
-    const planta = document.getElementById("planta").value;
+    alert("O botão foi acionado!");
 
-    const mensagem =
-        "Olá Fabiano! Vim pelo site da Arte & Flores Paisagismo. 🌿\n\n" +
-        "Nome: " + nome + "\n" +
-        "Planta que procuro: " + planta;
-
-    const link = "https://wa.me/554399961733?text=" + encodeURIComponent(mensagem);
-
-    window.open(link, "_blank");
 });
